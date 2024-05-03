2d91a85fdc2e15242d6d7e923e826f2ea72ed920
Andrey <xxxkeep3rxxx@gmail.com>
2024-05-04 00:12:48 +0300

    04.05

--- src/test/java/org/company/service/TaskTest.java
+++ src/test/java/org/company/service/TaskTest.java
@@ -1,16 +1,28 @@
 package org.company.service;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+import org.apache.log4j.spi.LoggingEvent;
 import org.company.bot.TelegramBot;
-import org.company.config.SpringConfig;
 import org.company.data.QuestionsLoader;
+import org.company.model.AnswerType;
 import org.company.model.Question;
 import org.company.model.TaskTags;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.telegram.telegrambots.meta.api.methods.send.SendVoice;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -18,34 +30,38 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@ExtendWith(MockitoExtension.class)
 public class TaskTest {
     @Mock
-    private TelegramBot bot;
-    private final Task task = new AnnotationConfigApplicationContext(SpringConfig.class).getBean(Task.class);
+    private Appender appenderMock;
+    @Mock
+    private TelegramBot botMock;
+    @Mock
+    private QuestionsLoader questionsLoaderMock;
+    private Task task;
 
-
+    @BeforeEach
+    public void init(){
+        task = new Task(botMock, questionsLoaderMock);
+    }
     @Test
     public void loadQuestionsTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        QuestionsLoader questionsLoader = Mockito.mock(QuestionsLoader.class);
-        task.setQuestionsLoader(questionsLoader);
         task.setTag(TaskTags.IAN_IANG_TAG);
 
         Method method = Task.class.getDeclaredMethod("loadQuestions");
         method.setAccessible(true);
         method.invoke(task);
-        Mockito.verify(questionsLoader).getQuestionList(TaskTags.IAN_IANG_TAG);
+        Mockito.verify(questionsLoaderMock).getQuestionList(TaskTags.IAN_IANG_TAG);
     }
 
     @Test
     public void loadUnitQuestionsTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        QuestionsLoader questionsLoader = Mockito.mock(QuestionsLoader.class);
-        task.setQuestionsLoader(questionsLoader);
         task.setTag(TaskTags.UNIT);
 
         Method method = Task.class.getDeclaredMethod("loadQuestions");
         method.setAccessible(true);
         method.invoke(task);
-        Mockito.verify(questionsLoader).getRandomQuestionList();
+        Mockito.verify(questionsLoaderMock).getRandomQuestionList();
     }
 
     @Test
@@ -67,16 +83,19 @@ public class TaskTest {
     }
 
     @Test
-    public void sendQuestionTest(){
+    public void sendQuestionTest() throws TelegramApiException {
         ArrayList<Integer> orderQuestions = new ArrayList<>();
         orderQuestions.add(0);
         List<Question> questionListMock = Mockito.mock(ArrayList.class);
-        SendVoice
-        Question question = new Question();
-        question.setMediaFiles(new ArrayList<>());
-        Mockito.doReturn().when(questionListMock).get(Mockito.anyInt());
+
+        Question question = new Question("chi-ci", "questionTxt", "answerA", "answerB", "answerC", "", "", "rightAnswer", new ArrayList<>(), AnswerType.CHOICE);
+        Mockito.doReturn(question).when(questionListMock).get(Mockito.anyInt());
 
         task.setORDER_QUESTIONS(orderQuestions);
+        task.setQuestions(questionListMock);
+        task.sendQuestion();
 
+        Mockito.verify(botMock).execute(Mockito.any(SendMessage.class));
     }
+
 }
