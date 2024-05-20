5b871e162bf8651bdb5d74fe9fff894fef4aac89
andreynikonov23 <xxxkeep3rxxx@gmail.com>
2024-05-21 00:29:53 +0300

    maven test compiling errors fix

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -6,6 +6,8 @@ module org.company.test {
     requires  org.mockito.junit.jupiter;
     requires  telegrambots.meta;
     requires  org.junit.platform.launcher;
+    requires java.xml.bind;
+    requires jakarta.activation;
 
     opens org.company.test.service to org.junit.platform.commons;
     opens org.company.test.data to org.junit.platform.commons;
